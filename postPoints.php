5ccf89f3da8ebdbe6f6f31e59b478bb79ef291a7
Chandler <chandlersterling1@gmail.com>
2014-11-11 19:10:41 +0300

    insert point to database function works!

--- postPoints.php
+++ postPoints.php
@@ -6,35 +6,29 @@
 	error_reporting(E_ALL);
 	
 
-	$host = "dev.indulgencelabs.com"; 
-	$user = "postgres"; 
-	$pass = "juanpfm!"; 
+	$host = "127.0.0.1"; 
+	$user = "lb_writer"; 
+	$pass = "lbwr1t3r"; 
 	$db = "littlebrother";
 
 	
 	$con = pg_connect("host=$host dbname=$db user=$user password=$pass")
 	or die ("Could not connect to server\n"); 
-		
-	$latlng = $_GET['latlng'];
+	
+	$lat = $_GET['lat'];
+	$lng = $_GET['lng'];
+	$point = $lat . ' ' . $lng; 
+	$description = $_GET['description'];	
 
-	$description = $_GET['description'];
-	//$point = $lat . ' ' . $lng;
-
-	$query = "INSERT INTO pending VALUES('" . $description . "', " . "'POINT(" . $latlng . ")');";
-
-	echo $query;
-	//echo($lat . " " . $lng);
+	$query = "INSERT INTO pending VALUES('POINT(" . $point . ")', '" . $description . "','no');";
 
 	//perform the insert using pg_query
-	//$result = pg_query($con, $query);
+	$result = pg_query($con, $query);
 
 	//dump the result object
 	var_dump($result);
 
 	// Closing connection
 	pg_close($con);
-	
-	
-	
 
 ?>
